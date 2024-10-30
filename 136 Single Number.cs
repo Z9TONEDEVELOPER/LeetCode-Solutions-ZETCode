82e6681e388a68a72098b87c88e66745d5dade22
Z9TONEDEVELOPER <maxalex1018@mail.ru>
2024-10-30 21:13:15 +0300

    Single Number

---
+++ 136 Single Number.cs
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/single-number/`
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int result=0;
+        foreach(int num in nums)
+        {
+            result ^= num;//XOR
+        }
+        return result;
+    }
+}
