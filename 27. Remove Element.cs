1a6b3c8168087e8b65bde4194d0213ffcfed38f3
Z9TONEDEVELOPER <maxalex1018@mail.ru>
2024-11-01 23:49:21 +0300

    27. Remove Element

---
+++ 27. Remove Element.cs
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/remove-element/
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int j=0;
+        for(int i=0;i<nums.Length; i++)
+        {
+            if(nums[i]!=val)
+            {
+                
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
+//0ms
